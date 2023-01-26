cb23c9679b66d7f5d8fa7c02a48a1ebea6eaf226
Fernando Aguiar <fernandoroberto95@gmail.com>
2023-01-25 21:51:53 -0300

    código inicial do projeto com tela de Login feita usando chatGPT

---
+++ android/app/src/main/kotlin/com/example/financeinhand/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.financeinhand
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,108 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class LoginApi {
+  final Uri _url = Uri.http('your-api.com', '/login');
+
+  Future<http.Response> login(String email, String password) async {
+    final Map<String, String> headers = {
+      'Accept': 'application/json',
+    };
+    final Map<String, String> body = {
+      'email': email,
+      'password': password,
+    };
+
+    final response =
+        await http.post(_url, headers: headers, body: jsonEncode(body));
+
+    return response;
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _loginApi = LoginApi();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor insira um e-mail válido';
+                  }
+                  if (!value.contains('@')) {
+                    return 'Por favor insira um e-mail válido';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Senha'),
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor insira uma senha válida';
+                  }
+                  if (!hasUppercase(value)) {
+                    return 'A senha deve conter pelo menos uma letra maiúscula';
+                  }
+                  if (!hasSpecialChar(value)) {
+                    return 'A senha deve conter pelo menos um caractere especial';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    final response = await _loginApi.login(
+                        _emailController.text, _passwordController.text);
+                    if (response.statusCode == 200) {
+                      print("Login realizado com sucesso!");
+                      print(response.body);
+                      // Aqui você pode navegar para a tela principal do aplicativo
+                    } else {
+                      print("Falha ao realizar login: ${response.body}");
+                    }
+                  }
+                },
+                child: const Text('Entrar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+bool hasUppercase(String value) {
+  return value.contains(RegExp(r'[A-Z]'));
+}
+
+bool hasSpecialChar(String value) {
+  return value.contains(RegExp(r'[!@#\$%^&*(),.?":{}|<>]'));
+}
